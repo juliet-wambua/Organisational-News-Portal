58d2ded44a6ef20badcfa2245cb1d44306755139
juliet-wambua <julienea289@gmail.com>
2019-08-09 17:26:19 +0300

    added create, read and delete methods to NewsDao.java file

--- src/main/java/dao/NewsDao.java
+++ src/main/java/dao/NewsDao.java
@@ -1,4 +1,18 @@
 package dao;
 
+import Models.News;
+
+import java.util.List;
+
 public interface NewsDao {
+    //create
+    void add(News news);
+
+    //read
+    List<News> getAll();
+    List<News> getAllNewsByDepartment(int departmentId);
+
+    //delete
+    void deleteById(int id);
+    void clearAll();
 }
