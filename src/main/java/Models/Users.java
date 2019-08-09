2426f791d0c8f0a81ee0ef0eba8ce105b1341cc1
juliet-wambua <julienea289@gmail.com>
2019-08-09 16:48:10 +0300

    created private modifiers for class user and constructors

--- src/main/java/Models/Users.java
+++ src/main/java/Models/Users.java
@@ -1,5 +1,20 @@
 package Models;
 
 public class Users {
+    private String userName;
+    private String userAddress;
+    private String userMobleNo;
+    private String userEmail;
+    private String position;
+    private int departmentId;
+
+    public Users(String userName, String userAddress, String userMobleNo,String userEmail, String position, int departmentId){
+       this.userName = userName;
+       this.userAddress = userAddress;
+       this.userMobleNo = userMobleNo;
+       this.userEmail = userEmail;
+       this.position = position;
+       this.departmentId = departmentId;
+    }
 
 }
