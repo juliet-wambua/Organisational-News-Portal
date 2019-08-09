898dc910ba90f08367eec7d35e9483ba110cef82
juliet-wambua <julienea289@gmail.com>
2019-08-09 17:21:33 +0300

    userDao.java interface

--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -1,4 +1,22 @@
 package dao;
 
+import org.h2.engine.User;
+
+import java.util.List;
+
 public interface UserDao {
+
+    //create
+    void add(User user);
+
+    //read
+    List<User> getAll();
+    // List<User> getAllUsersForADepartment(int id);
+
+    //update
+    //omit for now
+
+    //delete
+    void deleteById(int id);
+    void clearAll();
 }
