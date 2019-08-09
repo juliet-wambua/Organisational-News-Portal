fcc46f9a1d63626d91f8bc0acd0f1c1ca2fa2338
juliet-wambua <julienea289@gmail.com>
2019-08-09 16:04:15 +0300

    created department class and made private access modifiers for the department name, description and Number of employees and their constructors

--- src/main/java/Models/Departments.java
+++ src/main/java/Models/Departments.java
@@ -1,5 +1,16 @@
 package Models;
 
 public class Departments {
+    private String departmentName;
+    private String departmentDescription;
+    private String noOfEmployees;
+    private int id;
+
+    public Departments(String departmentName, String departmentDescription, String noOfEmployees){
+        this.departmentName = departmentName;
+        this.departmentDescription = departmentDescription;
+        this.noOfEmployees = noOfEmployees;
+
+    }
 
 }
