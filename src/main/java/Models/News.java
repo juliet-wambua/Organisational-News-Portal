8deff5170a3febd061882231ab9b1b188760db82
juliet-wambua <julienea289@gmail.com>
2019-08-09 16:33:53 +0300

    added private modifiers for departmentNews and  departmentId and their constructors

--- src/main/java/Models/News.java
+++ src/main/java/Models/News.java
@@ -1,5 +1,12 @@
 package Models;
 
 public class News {
+   private String generalNews;
+   private int departmentId;
+   private int id;
 
+ public News(String generalNews, int departmentId){
+    this.generalNews = generalNews;
+    this.departmentId = departmentId;
+ }
 }
