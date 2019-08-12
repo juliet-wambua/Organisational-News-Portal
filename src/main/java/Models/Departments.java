b7607a7dd3c58d90ea9428509ceec99ed5dd60ae
juliet-wambua <julienea289@gmail.com>
2019-08-12 07:01:31 +0300

    edit dao files

--- src/main/java/Models/Departments.java
+++ src/main/java/Models/Departments.java
@@ -13,4 +13,35 @@ public class Departments {
 
     }
 
+    public int getNoOfEmployees() {
+        return noOfEmployees;
+    }
+
+    public void setNoOfEmployees(int noOfEmployees) {
+        this.noOfEmployees = noOfEmployees;
+    }
+
+    public String getDepartmentName() {
+        return departmentName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setDepartmentName(String departmentName) {
+        this.departmentName = departmentName;
+    }
+
+    public String getDepartmentDescription() {
+        return departmentDescription;
+    }
+
+    public void setDepartmentDescription(String departmentDescription) {
+        this.departmentDescription = departmentDescription;
+    }
 }
--- src/main/java/dao/Sql2oDepartmentsDao.java
+++ src/main/java/dao/Sql2oDepartmentsDao.java
@@ -1,5 +1,24 @@
 package dao;
 
-public class Sql2oDepartmentsDao {
+import Models.Departments;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
 
+public class Sql2oDepartmentsDao {
+private final Sql2o sql2o;
+public Sql2oDepartmentsDao(Sql2o){
+    this.sql2o = sql2o;
+}
+
+@Override
+    public void add(Departments departments){
+    String sql = "INSERT INTO departments(departmentName, :departmentDescription, :noOfEmployees) VALUES (:departmentName, :departmentDescription, :noOfEmployees)";
+    try(Connection con = sql2o = sql2o.open()) {
+        int id = (int) con.createQuery(sql, true)
+                .bind(departments)
+                .executeUpdate()
+                .getKey();
+       departments.setId(id);
+    }
+}
 }
